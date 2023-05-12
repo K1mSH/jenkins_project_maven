01e1151da113d32cded8620da73c877898fa5cdf
K1mSH <124352219+K1mSH@users.noreply.github.com>
2023-05-12 13:47:18 +0900

    0147

---
+++ src/main/java/com/example/cloudaiservice2/CloudAiService2Application.java
@@ -0,0 +1,13 @@
+package com.example.cloudaiservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAiService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAiService2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudaiservice2/CloudAiService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudaiservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAiService2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
